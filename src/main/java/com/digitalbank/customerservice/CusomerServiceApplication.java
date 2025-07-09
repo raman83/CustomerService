eb3aa307105bb3b7d68e7839c63dd3eee4400b7c
mulesoftenthusiast <mulesoftenthusiast@gmail.com>
2025-07-09 00:06:06 -0400

    bug fixes

--- src/main/java/com/digitalbank/customerservice/CusomerServiceApplication.java
+++ src/main/java/com/digitalbank/customerservice/CusomerServiceApplication.java
@@ -3,9 +3,14 @@ package com.digitalbank.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = {
+        "com.digitalbank.customerservice",
+        "com.commons.security"
+})
 public class CusomerServiceApplication {
 
 	public static void main(String[] args) {
